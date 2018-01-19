8ac992665afffe51e9764953ae62b5619ac881f2
OliverDH <sunkxyz@gmail.com>
2018-01-19 17:37:16 +0800

    mysqlpy

---
+++ testMysql.py
@@ -0,0 +1,9 @@
+import pymysql
+
+conn = pymysql.connect(host='127.0.0.1',user='root', passwd='skx@326', db='mysql')
+cur = conn.cursor()
+cur.execute("USE scraping")
+cur.execute("SELECT * FROM pages WHERE id=1")
+print(cur.fetchone())
+cur.close()
+conn.close()
